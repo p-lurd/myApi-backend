7fac748f95e5f239f3990ad8e40da5f9fbb1f34e
Jedidiah <obayorit@gmail.com>
2025-04-01 08:42:34 +0100

    removed queing with redis

--- src/api/api-monitor.service.ts
+++ src/api/api-monitor.service.ts
@@ -8,62 +8,61 @@ import { CreateApiDto, CreateApiResponseDto } from './dto/create-api.dto';
 import Redis from 'ioredis';
 
 @Injectable()
-export class ApiMonitorService implements OnModuleInit {
+export class ApiMonitorService  {
   private readonly logger = new Logger(ApiMonitorService.name);
   private redisClient: Redis;
 
   constructor(
     @InjectModel('Api') private readonly apiModel: Model<ApiDocument>,
     @InjectModel(ApiResponseModelName) private readonly apiResponseModel: Model<ApiResponseDocument>,
-    private readonly redisService: RedisService
   ) {}
 
-  async fetchAndQueueApis() {
-    this.logger.log('Fetching APIs...');
-    const apis = await this.apiModel.find();
+  // async fetchAndQueueApis() {
+  //   this.logger.log('Fetching APIs...');
+  //   const apis = await this.apiModel.find();
 
-    for (const api of apis) {
-      await this.redisClient.lpush('api_jobs', JSON.stringify(api));
-    }
+  //   for (const api of apis) {
+  //     await this.redisClient.lpush('api_jobs', JSON.stringify(api));
+  //   }
 
-    this.logger.log(`Queued ${apis.length} APIs for processing.`);
-  }
+  //   this.logger.log(`Queued ${apis.length} APIs for processing.`);
+  // }
 
-  async onModuleInit() {
+  // async onModuleInit() {
 
-    // CHANGES HERE
-    this.redisClient = this.redisService.getOrThrow();
-    this.listenForResults();
-  }
+  //   // CHANGES HERE
+  //   this.redisClient = this.redisService.getOrThrow();
+  //   this.listenForResults();
+  // }
 
-  async listenForResults() {
-    this.redisClient.subscribe('api_results', (err, count) => {
-      if (err) {
-        this.logger.error('Failed to subscribe to api_results:', err);
-      } else {
-        this.logger.log(`Subscribed to ${count} channels.`);
-      }
-    });
+  // async listenForResults() {
+  //   this.redisClient.subscribe('api_results', (err, count) => {
+  //     if (err) {
+  //       this.logger.error('Failed to subscribe to api_results:', err);
+  //     } else {
+  //       this.logger.log(`Subscribed to ${count} channels.`);
+  //     }
+  //   });
 
-    this.redisClient.on('message', async (channel, message) => {
-        try {
-            if (channel === 'api_results') {
-                const result = JSON.parse(message);
-                console.log({result})
-                // await this.apiModel.findByIdAndUpdate(result.id, { status: result.status });
-                await this.apiResponseModel.create(result)
-                this.logger.log(`Stored API result: ${result.id} - ${result.status}`);
-              }
-        } catch (error) {
-            if(error instanceof HttpException){
-                // return res.status(500).json({message: error.message})
-                return error
-            }
-            throw new InternalServerErrorException(error.message);
-        }
+  //   this.redisClient.on('message', async (channel, message) => {
+  //       try {
+  //           if (channel === 'api_results') {
+  //               const result = JSON.parse(message);
+  //               console.log({result})
+  //               // await this.apiModel.findByIdAndUpdate(result.id, { status: result.status });
+  //               await this.apiResponseModel.create(result)
+  //               this.logger.log(`Stored API result: ${result.id} - ${result.status}`);
+  //             }
+  //       } catch (error) {
+  //           if(error instanceof HttpException){
+  //               // return res.status(500).json({message: error.message})
+  //               return error
+  //           }
+  //           throw new InternalServerErrorException(error.message);
+  //       }
       
-    });
-  }
+  //   });
+  // }
 
 //   route to create apis
 async createApi (createApiDto: CreateApiDto){
--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiMonitorService } from './api-monitor.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RedisConfig } from 'src/redis/redis.config';
 import { ApiModelName, ApiSchema } from './schemas/api.schema';
 import { ApisController } from './api.controller';
 import { ApiResponseModelName, ApiResponseSchema } from './schemas/apiResponse.schema';
 
 @Module({
   imports: [
-    RedisConfig,
     MongooseModule.forFeature([
     { name: ApiModelName, schema: ApiSchema },
     {name: ApiResponseModelName, schema: ApiResponseSchema}
