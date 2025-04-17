50cdeed54a4b51a740f5e39385e7c59abdde8462
Jedidiah <obayorit@gmail.com>
2025-04-17 20:43:57 +0100

    added production variables, removed redis byproducts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  app.enableCors(); // Allow CORS
-  await app.listen(3000);
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  }); // Allow CORS
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
