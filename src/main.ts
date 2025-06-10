c97bc039209d637fcba2800d3473bf3e22f74095
Jedidiah <obayorit@gmail.com>
2025-06-10 15:03:17 +0100

    port changes

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,13 @@ async function bootstrap() {
   // }); // Allow CORS
 
   // Custom CORS
-  const allowedOrigins = configService.get<string>('allowedOrigins')?.split(',') || ['http://localhost:5173'];
+  const allowedOrigins = configService
+    .get<string>('allowedOrigins')
+    ?.split(',') || ['http://localhost:5173'];
 
-app.use(customCorsMiddleware(allowedOrigins));
-  await app.listen(configService.get<string>('port') || 8080, '0.0.0.0');
+  app.use(customCorsMiddleware(allowedOrigins));
+  const port = process.env.PORT || configService.get<string>('port') || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`server running on port:${configService.get<string>('port')}`);
 }
 bootstrap();
